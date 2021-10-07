02c311066df16fb68639921260ed55e79e8a0a4b
enricco-bertazzo <you@example.com>
2021-10-07 17:17:54 -0300

    Remodulação Front-end

--- src/pages/cadastro.php
+++ src/pages/cadastro.php
@@ -4,11 +4,25 @@
         <title> Sistema de Acompanhamento </title>
         <link rel="stylesheet" href="../css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	    <meta charset="utf-8">
+	    <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	    <link href="../css/style.css" rel="stylesheet">
+	    <link href="../css/formulario-dados.css" rel="stylesheet">
+	    <script src="../js/popper.min.js"></script>
+	    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+	    <script src="https://kit.fontawesome.com/0dc26a975a.js" crossorigin="anonymous"></script>
+	    <script src="../js/jquery-3.6.0.min.js"></script>
+	    <script src="../js/jquery.mask.min.js"></script>
     </head>
 
     <body class="body text-center">
-        <form class="cad" action="../php/insert.php" method="POST">
-            <div>
+        <?php
+
+        include "menu.php";
+
+        ?>
+        <form action="../php/insert.php" method="POST">
+            <div class="card" id="formulario">
                 <div class="form-floating">
                     <input name="cadastro" placeholder="Nº do Cadastro" type="text" class="form-control" required>
                     <label> Nº do Cadastro </label>
@@ -71,7 +85,6 @@
                 </div>
 
                 <button class="btn w-100 btn-primary" type="submit"> Cadastrar </button>
-                <p class="mt-5 mb-3" > Deseja pesquisar? <a href="consulta.php"> Pesquise! </a></p>
             </div>
             
 		</form>
---
+++ src/pages/dashboard.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+	<title> Menu </title>
+	<meta charset="utf-8">
+	<link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+	<script src="https://kit.fontawesome.com/0dc26a975a.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<?php
+
+	include "menu.php";
+
+	?>
+</body>
+
+</html>
