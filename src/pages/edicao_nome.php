31501b65245326d9622c2689032bdb47c4315d2d
enricco-bertazzo <you@example.com>
2021-10-08 15:59:56 -0300

    save

--- src/pages/edicao_nome.php
+++ src/pages/edicao_nome.php
@@ -5,14 +5,14 @@
         <link rel="stylesheet" href="../css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 	    <meta charset="utf-8">
-	    <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
-	    <link href="../css/style.css" rel="stylesheet">
-	    <link href="../css/formulario-dados.css" rel="stylesheet">
-	    <script src="../js/popper.min.js"></script>
-	    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
+	    <link href="../edicao/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+	    <link href="../edicao/css/style.css" rel="stylesheet">
+	    <link href="../edicao/css/formulario-dados.css" rel="stylesheet">
+	    <script src="../edicao/js/popper.min.js"></script>
+	    <script src="../edicao/js/bootstrap/bootstrap.bundle.min.js"></script>
 	    <script src="https://kit.fontawesome.com/0dc26a975a.js" crossorigin="anonymous"></script>
-	    <script src="../js/jquery-3.6.0.min.js"></script>
-	    <script src="../js/jquery.mask.min.js"></script>
+	    <script src="../edicaojs/jquery-3.6.0.min.js"></script>
+	    <script src="../edicao/js/jquery.mask.min.js"></script>
     </head>
 
     <body class="body text-center">
--- src/pages/edit_query_endereco.php
+++ src/pages/edit_query_endereco.php
@@ -4,14 +4,14 @@
         <title> Sistema de Acompanhamento </title>
         <link rel="stylesheet" href="../css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-        <meta charset="utf-8">
+	    <meta charset="utf-8">
 	    <link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet">
 	    <link href="../css/style.css" rel="stylesheet">
 	    <link href="../css/formulario-dados.css" rel="stylesheet">
 	    <script src="../js/popper.min.js"></script>
 	    <script src="../js/bootstrap/bootstrap.bundle.min.js"></script>
 	    <script src="https://kit.fontawesome.com/0dc26a975a.js" crossorigin="anonymous"></script>
-	    <script src="../js/jquery-3.6.0.min.js"></script>
+	    <script src="..js/jquery-3.6.0.min.js"></script>
 	    <script src="../js/jquery.mask.min.js"></script>
     </head>
     <body>
